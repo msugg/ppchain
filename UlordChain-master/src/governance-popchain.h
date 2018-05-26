548e64a9e2e5050ad9e88ca0fa2aea677f968097
arminsu7 <734722864@qq.com>
2018-05-26 15:18:46 +0800

    office

--- UlordChain-master/src/governance-popchain.h
+++ UlordChain-master/src/governance-popchain.h
@@ -1,6 +1,4 @@
-// Copyright (c) 2014-2017 The Dash Core developers
-// Distributed under the MIT/X11 software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef GOVERNANCE_POPCHAIN_H
 #define GOVERNANCE_POPCHAIN_H
 
@@ -13,7 +11,7 @@ private:
 
 public:
     testpopchain(int a):a(a){}
-    int ra(){return a;}
+    int ra();
 };
 
 
