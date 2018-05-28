e764af73e44bf6e5ea4e17394bbb84852a9aabe0
arminsu7 <734722864@qq.com>
2018-05-28 16:48:36 +0800

    office

--- UlordChain-master/src/governance.h
+++ UlordChain-master/src/governance.h
@@ -18,18 +18,18 @@
 #include "sync.h"
 #include "timedata.h"
 #include "util.h"
-#include <limits>
+
 
 // popchain dont need it
-//class CGovernanceManager;
-//class CGovernanceTriggerManager;
-//class CGovernanceObject;
-//class CGovernanceVote;
+class CGovernanceManager;
+class CGovernanceTriggerManager;
+class CGovernanceObject;
+class CGovernanceVote;
 
-//extern std::map<uint256, int64_t> mapAskedForGovernanceObject;
-//extern CGovernanceManager governance;
+extern std::map<uint256, int64_t> mapAskedForGovernanceObject;
+extern CGovernanceManager governance;
 
-//typedef std::pair<CGovernanceObject, int64_t> object_time_pair_t;
+typedef std::pair<CGovernanceObject, int64_t> object_time_pair_t;
 
 static const int RATE_BUFFER_SIZE = 5;
 
