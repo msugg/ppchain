e7d7006a9e32da89ce6bf4d426ea9f1cd264bfd1
sumingrui <sumingrui@gmail.com>
2018-06-02 02:39:39 +0800

    office

--- UlordChain-master/src/test/miner_tests.cpp
+++ UlordChain-master/src/test/miner_tests.cpp
@@ -78,8 +78,6 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     LOCK(cs_main);
     fCheckpointsEnabled = false;
 
-    // force UpdatedBlockTip to initialize pCurrentBlockIndex
-
     // Simple block creation, nothing special yet:
     BOOST_CHECK(pblocktemplate = CreateNewBlock(chainparams, scriptPubKey));
 
